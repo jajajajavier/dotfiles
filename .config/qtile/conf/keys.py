0aa8d89f5993d19c3c04d90ed159ba6b03e8c7f0
jajajajavier <71715217+jajajajavier@users.noreply.github.com>
2021-04-18 01:06:37 +0000

    Add files via upload

---
+++ .config/qtile/conf/keys.py
@@ -0,0 +1,131 @@
+# Keys Config
+#
+# por javier
+# https://github.com/jajajajavier
+
+
+from libqtile.config import Key, Drag, Click
+from libqtile.lazy import lazy
+
+
+mod = "mod4"                    # Tecla windows
+Alt = "mod1"                    # Tecla Alt izquierda 
+
+
+#-------------------- combinaciones de teclado-------------------- 
+
+# En desc= estan las teclas y combinaciones
+# Arriba de cada configuracion una breve explicacion
+
+keys = [
+    
+    # >>>>>>>>>>>>>>>|Ventanas|<<<<<<<<<<<<<<<
+
+
+    # cambia distribucion de ventanas. Para agregar o quitar distribuciones editar: ~/.config/qtile/conf/layout.py
+    Key([mod, "shift"], "Tab", lazy.next_layout(),              desc="Win + Shift + Tab"),
+    
+    # Mueve la ventana a otra posicion, sea arriba, abajo, izquierda o derecha
+    Key([mod, "shift"], "Left", lazy.layout.shuffle_left(),     desc="Win + Shift + Flecha izquierda"),
+    Key([mod, "shift"], "Right", lazy.layout.shuffle_right(),   desc="Win + Shift + Flecha derecha"),
+    Key([mod, "shift"], "Down", lazy.layout.shuffle_down(),     desc="Win + Shift + Flecha abajo"),
+    Key([mod, "shift"], "Up", lazy.layout.shuffle_up(),         desc="Win + Shift + Flecha arriba"),
+
+    # Redimensiona la ventana, columna o fila hacia cualquier direccion 
+    Key([mod, "control"], "Left", lazy.layout.grow_left(),      desc="Win + Control izq. + Flecha izquierda"),
+    Key([mod, "control"], "Right", lazy.layout.grow_right(),    desc="Win + Control izq. + Flecha derecha"),
+    Key([mod, "control"], "Down", lazy.layout.grow_down(),      desc="Win + Control izq. + Flecha abajo"),
+    Key([mod, "control"], "Up", lazy.layout.grow_up(),          desc="Win + Control izq. + Flecha arriba"),
+    
+    # Cambiar la ventana enfocada a arriba, izquierda, derecha o abajo de esta 
+    Key([mod], "Left", lazy.layout.left(),                      desc="Win + Flecha izquierda"),
+    Key([mod], "Right", lazy.layout.right(),                    desc="Win + Flecha derecha"),
+    Key([mod], "Down", lazy.layout.down(),                      desc="Win + Flecha abajo"),
+    Key([mod], "Up", lazy.layout.up(),                          desc="Win + Flecha arriba"),
+
+    # Tabear a la ventana siguiente
+    Key([Alt], "Tab", lazy.layout.next(),                       desc="Alt + Tab"),
+
+    # Redimensiona las ventanas a su estado normal 
+    Key([mod], "Delete", lazy.layout.normalize(),               desc="Win + Suprimir"),
+
+    # Cierra la ventana enfocada
+    Key([mod], "q", lazy.window.kill(),                         desc="Win + q"),
+
+    # Traer una ventana al frente. La saca del mosaico 
+    Key([mod], "Prior", lazy.window.bring_to_front(),           desc="Win + Re pag "),
+    
+    # Ventana completa
+    Key([mod], "F11", lazy.window.toggle_fullscreen(),          desc="Win + F11"),
+
+    # Poner ventana flotante en el mosaico 
+    Key([mod], "Next", lazy.window.toggle_floating(),           desc="Win + Av pag"),
+
+    
+    # >>>>>>>>>>>>>>>|Qtile|<<<<<<<<<<<<<<<
+    
+
+    # Reiniciar Qtile
+    Key([mod, "control"], "r", lazy.restart(),                  desc="Win + Control izq. + r"),
+
+    # Apagar Qtile
+    Key([mod, "control"], "Pause", lazy.shutdown(),             desc="Win + Control izq. + Pause"),
+    
+     
+    # >>>>>>>>>>>>>>>|Audio y brillo|<<<<<<<<<<<<<<<
+
+   
+    # Audio, pamixer
+
+    # Bajar volumen  
+    Key([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 2"),     desc="Fn + Flecha abajo"),
+
+    # Subir volumen 
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("pamixer --increase 2"),     desc="Fn + Flecha arriba"),
+
+
+    # Brillo, xbacklight 
+
+    # Subir brillo un 5%
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set +5%"),   desc="Fn + Flecha izquierda"),
+
+    # Bajar brillo un 5%
+    Key([], "XF86MonBrightnessUp", lazy.spwan("brightnessctl set +5%"),     desc="Fn + Flecha derecha"),
+
+
+   # >>>>>>>>>>>>>>>|Otros|<<<<<<<<<<<<<<<
+   
+   
+    # Terminal Alacritty 
+    Key([mod], "t", lazy.spawn("alacritty"),                                desc="Win + T"),
+    
+    # Menu de ventanas
+    Key([mod, "shift"], "Return", lazy.spawn("rofi -show"),                 desc="Win + Shift + Enter"),
+    
+    # arrancar programas
+    Key([mod], "Return", lazy.spawn("rofi -show run"),                      desc="Win + Enter"),
+    
+    # Firefox
+    Key([mod], "b", lazy.spawn("firefox"),                                  desc="Win + b"),
+    
+    # Gestor de archivos ( Ranger )
+    Key([mod], "BackSpace", lazy.spawn("alacritty --command ranger"),       desc="Win + Borrar"),
+    
+    # screenshoot
+    Key([mod, "shift"], "s", lazy.spawn("xfce4-screenshooter -r"),          desc="Win + Shift + s"), 
+]
+
+
+# >>>>>>>>>>>>>>>|Mouse|<<<<<<<<<<<<<<<
+
+# Sacar y redimensionar ventana
+mouse = [
+    
+    # Flota la ventana
+    Drag([mod, "shift"], "Button1", lazy.window.set_position_floating(),    desc="Win + Shift + Click izq.",
+        start=lazy.window.get_position()),
+    
+    # Redimensionar la ventana flotante
+    Drag([mod, "shift"], "Button3", lazy.window.set_size_floating(),        desc="Win + Shift + Click der.",
+        start=lazy.window.get_size()),
+]
---
+++ .config/qtile/config.py
@@ -0,0 +1,61 @@
+# Qtile config por javier 
+# https://github.com/jajajajavier
+#
+# archivo de configuracion: 
+# http://qtile.org
+#
+# hecho en arch linux 
+
+# -------------------|Imports|--------------------
+
+from libqtile import hook
+from os import path
+import os
+import re
+import socket
+import subprocess
+
+# ---------------|Config imports|--------------- 
+
+from conf.keys import mod, keys, mouse
+from conf.groups import groups
+from conf.layout import layouts, floating_layout
+from conf.widget import widget_defaults, extension_defaults
+from conf.screen import screens
+from conf.path import qtile_path
+
+# ------------------------------------------------
+
+@hook.subscribe.startup_once
+def autostart():
+    subprocess.call([path.join(qtile_path, 'autostart.sh')])
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+main = None  # WARNING: this is deprecated and will be removed soon
+follow_mouse_focus = False
+bring_front_click = False
+cursor_warp = False
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+wmname = "LG3D"
+
+# Este archivo de configuracion se hizo con la info de la documentacion de Qtile
+# para todas las veces que la "cage" en algo me ayudo mucho ver los archivos de
+# Derek Taylor tiene un canal de youtube 100% recomendado, y el archivo  de configuracion 
+# de Antonio Sarosi, tambien con un canal en youtube, mas que nada para tomar su archivo themes.py
+# porque estaba haciendo algo para cambiar temas de colores pero se estaba llenando de codigo basura
+# y errores, y con el suyo andaba de pana
+#
+#
+# CREDITS=
+#
+# Derek Taylor ( Distro Tube) :
+#   Youtube: https://youtube.com/distrotube
+#   Gitlab: https://gitlab.com/dwt1
+#
+# Antonio Sarosi : 
+#   youtube: https://youtube.com/antoniosarosi
+#   Github: https://github.com/antoniosarosi
+#
+# Pasate por sus canales no seas puto
