43aa0587d2aa23aced464ae3ecd9cd1a37f2637c
jajajajavier <jcalfv@gmail.com>
2024-10-12 19:56:07 -0300

    added script for view current playing track

--- .config/xmobar/xmobar.hs
+++ .config/xmobar/xmobar.hs
@@ -11,17 +11,19 @@ config = defaultConfig {
   allDesktops = True,
   persistent = True,
   commands = [ 
-    Run $ Com "echo" ["<fc=#4C566A>\61523</fc>"]  "sep" 1000000000,
+    Run $ Com "echo" ["<fc=#4C566A>\61523</fc>"]  "sepL" 1000000000,
+    Run $ Com "echo" ["<fc=#4C566A>\61524</fc>"]  "sepR" 1000000000,
     Run $ Com "battery.sh" [] "batt" 200,
     Run $ Com "audio.sh" [] "volume" 5,
     Run $ Com "brightness.sh" [] "brightness" 5,
+    Run $ Com "music.sh" [] "music" 5,
     Run $ Date "<fc=#61AFEF>\62635 %H:%M</fc>" "time" 10,
     Run $ Date "<fc=#56B6C2>\989744 %a, %d %b</fc>" "date" 100000,
     Run UnsafeStdinReader
   ],
   sepChar = "%",
   alignSep = "}{",
-  template = " %UnsafeStdinReader% }{ %sep% %volume% %sep% %brightness% %sep% %batt% %sep% %date% %sep% %time% "
+  template = " %UnsafeStdinReader% %sepR% %music% }{ %sepL% %volume% %sepL% %brightness% %sepL% %batt% %sepL% %date% %sepL% %time% "
 }
 
 main :: IO()
